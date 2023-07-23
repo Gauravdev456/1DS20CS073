67ca80d35f024074e16e4fafe7c50aa0d86328e5
Gauravdev HB <gauravdev818@gmail.com>
2023-07-23 11:47:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Prime from './components/prime'
+
+function App() {
+  return (
+    <Prime>
+      
+    </Prime>
+  );
+}
+
+export default App;
---
+++ src/components/prime.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Prime() {
+    let API = "http://20.244.56.144/numbers/primes";
+    let API2="http://20.244.56.144/numbers/fibo";
+    const [primeNumbers, setPrimeNumbers] = useState([]);
+    const [fibNumbers, setfibNumbers] = useState([]);
+
+    const fetchApiData = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setPrimeNumbers(data.numbers); 
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const fetchApiData2 = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setfibNumbers(data.numbers);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchApiData(API);
+    }, [API]);
+
+    useEffect(() => {
+        fetchApiData2(API2);
+    }, [API2]);
+    
+
+    
+
+    return (
+        <>
+            <h1>Prime Numbers</h1>
+            <ul>
+                {primeNumbers.map((number, index) => (
+                    <li key={index}>{number}</li>
+                ))}
+            </ul>
+            <h1>Fibonacci Numbers</h1>
+            <ul>
+                {fibNumbers.map((number, index) => (
+                    <li key={index}>{number}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
